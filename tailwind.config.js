73f5d4998b674e9c3930723d375e07665f723154
YourName <you@example.com>
2025-10-11 11:01:50 +0530

    dark mode update 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html"
   ],
   darkMode: 'class',
   theme: {
@@ -24,16 +25,16 @@ export default {
       },
       keyframes: {
         fadeIn: {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+          from: { opacity: '0' },
+          to: { opacity: '1' },
         },
         slideUp: {
           from: { 
-            opacity: 0,
+            opacity: '0',
             transform: 'translateY(30px)'
           },
           to: { 
-            opacity: 1,
+            opacity: '1',
             transform: 'translateY(0)'
           },
         }
