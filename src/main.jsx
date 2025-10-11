53264efce11f5f636c9a02785f012c147a5d1222
Waris Rawa <WARISRAWA145@GMAIL.COM>
2025-10-11 11:47:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './Index.css'  // Regular CSS import, NOT from './styles/index.css'
+import './index.css'  // Changed to lowercase!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
